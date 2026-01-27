def5189ae09f2e763f3a5f1762f34e602ff2cabd
Alexander Shevchenko <aleksandr.shevchenko@gmail.com>
2026-01-27 21:10:54 +0200

    Minor fixes

--- src/main/java/com/zmpc/util/App.java
+++ src/main/java/com/zmpc/util/App.java
@@ -2,11 +2,10 @@ package com.zmpc.util;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(">> my-common-utils project :");
+        //System.out.println(">> my-common-utils project :");
         System.out.println(" - StringUtils");
         System.out.println(" - ObjectUtils");
         System.out.println(" - FileUtils");
-        System.out.println(" - PrintUtils");
     }
 
 }
