4a113aa1df7947611f1b7ab050ac5fc23fb9e06b
Alexander Shevchenko <aleksandr.shevchenko@gmail.com>
2026-01-28 01:44:05 +0200

    uncomment line

--- src/main/java/com/zmpc/util/App.java
+++ src/main/java/com/zmpc/util/App.java
@@ -2,7 +2,7 @@ package com.zmpc.util;
 
 public class App {
     public static void main(String[] args) {
-        //System.out.println(">> my-common-utils project :");
+        System.out.println(">> my-common-utils project :");
         System.out.println(" - StringUtils");
         System.out.println(" - ObjectUtils");
         System.out.println(" - FileUtils");
