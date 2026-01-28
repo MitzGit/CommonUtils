3f3381de50344a8f034994e323c41f47216379ce
Alexander Shevchenko <aleksandr.shevchenko@gmail.com>
2026-01-28 02:50:30 +0200

    add splitByIndex()

--- src/main/java/com/zmpc/util/StringUtils.java
+++ src/main/java/com/zmpc/util/StringUtils.java
@@ -18,4 +18,19 @@ public class StringUtils {
     public static String emptyIfNull(String str) {
         return str == null ? "" : str;
     }
+
+    public static String[] splitByIndex(String str, int index) {
+        if (isNullOrEmpty(str)) {
+            String[] emptyParts = {"", ""};
+            return emptyParts;
+        }
+        if (index >= 0 && index < str.length()) {
+            String[] parts = new String[2];
+            parts[0] = str.substring(0, index);
+            parts[1] = str.substring(index);
+            return parts;
+        }
+
+        return new String[]{str};
+    }
 }
